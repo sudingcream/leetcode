3900d59baadd4c5d26d10f011f3c5ee5657409d2
suding <79178843+sudingcream@users.noreply.github.com>
2021-03-15 18:19:06 +0900

    Create TwoSum.cpp
    LeetCode 1.TwoSum

---
+++ algorithms/TwoSum.cpp
@@ -0,0 +1,16 @@
+// LeeCode 
+// 1. TwoSum
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int sum = 0; vector<int> result;
+        for(int i=0; i<nums.size();i++){
+           sum = nums[i] + nums[i+1];
+            if(sum == target){
+               return {i,i+1};
+            }
+        }// end for 
+        return {};
+    }
+};
