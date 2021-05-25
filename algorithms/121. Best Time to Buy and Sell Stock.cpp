e7b62ec47a4e736d7c76f5e4b4286d4e32d81745
suding <79178843+sudingcream@users.noreply.github.com>
2021-05-25 14:24:31 +0900

    Create 121. Best Time to Buy and Sell Stock.cpp
    121. Best Time to Buy and Sell Stock

---
+++ algorithms/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max=0, min=prices[0];
+        
+        for(int i=0;i<prices.size();i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }
+                
+            if((prices[i] - min) > max){
+                max = prices[i] - min;
+            }
+                
+        }// end for 
+        return max;
+    }
+};
+
